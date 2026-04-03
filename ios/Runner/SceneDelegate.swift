cb041c54227ae90bd7fd7ef3f63fb468906d5f56
Gaurav Rathi <g.rathi@sap.com>
2026-04-02 18:32:58 -0700

    maps

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -1,6 +1,14 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 class SceneDelegate: FlutterSceneDelegate {
-
+  override func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    GMSServices.provideAPIKey("AIzaSyD9qpVoecA0DQzfoeVKSiBD2VPz4xa_frk")
+    super.scene(scene, willConnectTo: session, options: connectionOptions)
+  }
 }
